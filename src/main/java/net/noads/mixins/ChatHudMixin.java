0e777c7805d0b162b70197f72682962aec060c66
5EUS <samsonite04@protonmail.com>
2021-11-09 23:16:57 -0500

    Added mixin to cancel messages that start with "[AD]"

---
+++ src/main/java/net/noads/mixins/ChatHudMixin.java
@@ -0,0 +1,21 @@
+package net.noads.mixins;
+
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChatHud.class)
+public abstract class ChatHudMixin {
+
+    @Inject(at = @At("HEAD"), method = "addMessage", cancellable = true)
+    private void addMessage(Text message, CallbackInfo info) {
+        if (message.asString().startsWith("[AD]")) {
+            info.cancel();
+        }
+    }
+
+}
+
